68ae3aaa2dec1e3cbf660636284b0c0718c60150
jadmerheb98 <jadmerheb1998@gmail.com>
2026-03-01 03:18:46 +0200

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,14 +1,18 @@
 // js/firebase-init.js
-// 1) Paste your firebaseConfig from Firebase Console here.
+// Firebase init (CDN compat) for PADELIN
+
 const firebaseConfig = {
-  apiKey: "PASTE",
-  authDomain: "PASTE",
-  projectId: "PASTE",
-  appId: "PASTE",
+  apiKey: "AIzaSyA0WZUyKoNjlOzmEeEhPLALKsTmNmazBVE",
+  authDomain: "padelin-9bc6d.firebaseapp.com",
+  projectId: "padelin-9bc6d",
+  storageBucket: "padelin-9bc6d.firebasestorage.app",
+  messagingSenderId: "811369149103",
+  appId: "1:811369149103:web:b69f74bbfe8adb22145a5a",
+  measurementId: "G-0EDTQGB711"
 };
 
-// 2) Initialize Firebase (compat SDK so it works with your current non-module main.js)
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// 3) Expose auth
+// Expose auth globally
 window.padelinAuth = firebase.auth();
