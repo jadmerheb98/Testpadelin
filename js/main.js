7294a92e850dcff5fb4118ace4ba3e18c40daa9d
jadmerheb98 <jadmerheb1998@gmail.com>
2026-03-16 09:46:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -223,8 +223,11 @@ window.location.href = "index.html";
 
 if (name) await cred.user.updateProfile({ displayName: name });
 
-if (db) {
-  await db.collection("users").doc(cred.user.uid).set({
+if (!db) {
+  throw new Error("Firestore is not loaded on signup page.");
+}
+
+await db.collection("users").doc(cred.user.uid).set({
   uid: cred.user.uid,
   name: name || "",
   email: email || "",
@@ -234,7 +237,7 @@ if (db) {
   updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
   createdAt: firebase.firestore.FieldValue.serverTimestamp(),
 }, { merge: true });
-}
+            
             localStorage.setItem("padelinUser", JSON.stringify({
               uid: cred.user.uid,
               email: email || "",
