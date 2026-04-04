a35c70f47689203c7746616e7032b3452cba4fd2
jadmerheb98 <jadmerheb1998@gmail.com>
2026-04-04 09:16:59 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -168,6 +168,14 @@ if (dn !== "STAFF:ADMIN") {
 }
 
 // Normal login
+const postLoginRedirect = localStorage.getItem("padelinPostLoginRedirect");
+
+if (postLoginRedirect) {
+  localStorage.removeItem("padelinPostLoginRedirect");
+  window.location.href = postLoginRedirect;
+  return;
+}
+
 window.location.href = "index.html";
           } catch (err) {
 
@@ -284,7 +292,15 @@ await db.runTransaction(async (transaction) => {
 
             localStorage.setItem("padelinRewardPoints", localStorage.getItem("padelinRewardPoints") || "0");
 
-            window.location.href = "index.html";
+            const postLoginRedirect = localStorage.getItem("padelinPostLoginRedirect");
+
+if (postLoginRedirect) {
+  localStorage.removeItem("padelinPostLoginRedirect");
+  window.location.href = postLoginRedirect;
+  return;
+}
+
+window.location.href = "index.html";
           } catch (err) {
             alert(err.message);
           }
